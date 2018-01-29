acc06f9dfa003bb8c1bf54ef04bc01d40a4aeae1
Nicholas Wong <lion9200.world@gmail.com>
2018-01-29 15:43:31 +0100

    add quill theme

--- src/plugins/vue-directus/components/modules/Text.vue
+++ src/plugins/vue-directus/components/modules/Text.vue
@@ -49,23 +49,23 @@ export default {
     changed({ html, text }) {
       // We need to compare trimmed texts for the case that the initial content
       // we got from the cms was a plain string instead of html
-      if (html !== this.initialContent && text.trim() !== this.initialContent.trim()) {
-        this.$store.dispatch('VueDirectus/items/edit', {
-          table: this.$parent.$props.table,
-          id: this.$parent.$props.id,
-          column: this.$props.column,
-          content: html
-        })
-        this.$store.dispatch('VueDirectus/items/commit', {
-          table: this.$parent.$props.table,
-          id: this.$parent.$props.id
-        })
-      } else {
-        this.$store.dispatch('VueDirectus/items/undo', {
-          table: this.$parent.$props.table,
-          id: this.$parent.$props.id
-        })
-      }
+      // if (html !== this.initialContent && text.trim() !== this.initialContent.trim()) {
+      //   this.$store.dispatch('VueDirectus/items/edit', {
+      //     table: this.$parent.$props.table,
+      //     id: this.$parent.$props.id,
+      //     column: this.$props.column,
+      //     content: html
+      //   })
+      //   this.$store.dispatch('VueDirectus/items/commit', {
+      //     table: this.$parent.$props.table,
+      //     id: this.$parent.$props.id
+      //   })
+      // } else {
+      //   this.$store.dispatch('VueDirectus/items/undo', {
+      //     table: this.$parent.$props.table,
+      //     id: this.$parent.$props.id
+      //   })
+      // }
     }
   }
 }
