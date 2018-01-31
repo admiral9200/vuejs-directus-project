eef55c32ea96716d59e9abd4ff44a404db40aaba
Nicholas Wong <lion9200.world@gmail.com>
2018-01-31 20:34:41 +0100

    add fetch action and getter

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <vue-directus-app>
       <h1>Vue Directus Collection `projects`</h1>
       <vue-directus-collection>
-        <vue-directus-item>
-          <h2>Integer ornare</h2>
+        <vue-directus-item v-for="project in itemsByTable('projects')" :key="project.id">
+          <h2>{{ project.name }}</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis molestie ipsum.</p>
         </vue-directus-item>
       </vue-directus-collection>
@@ -13,8 +13,26 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  computed: {
+    ...mapGetters({
+      itemsByTable: 'VueDirectus/items/itemsByTable'
+    })
+  },
+
+  created() {
+    this.fetch('projects')
+  },
+
+  methods: {
+    ...mapActions({
+      fetch: 'VueDirectus/items/fetch'
+    })
+  }
 }
 </script>
 
--- src/plugins/vue-directus/store/modules/items.js
+++ src/plugins/vue-directus/store/modules/items.js
@@ -1,20 +1,40 @@
 /* eslint-disable padding-line-between-statements */
+import VueDirectusApi from '../../api'
+
 const namespaced = true
 
 const state = {
-  //
+  fetched: {}
 }
 
 const mutations = {
-  //
+  FETCH: (state, payload) => {
+    state.fetched = payload
+  }
 }
 
 const actions = {
-  //
+  // Fetch item by table and
+  // save items with table name as key
+  async fetch({ commit }, table) {
+    await VueDirectusApi.getItems(table)
+      .then(resp => {
+        const data = {}
+        data[table] = resp
+        return commit('FETCH', data)
+      })
+      .catch(() => {
+        throw Error('Failed to fetch items from directus backend')
+      })
+  }
 }
 
 const getters = {
-  //
+  // Get all items by table and return
+  // a function so we can pass table name as an argument
+  itemsByTable(state) {
+    return table => (state.fetched[table] ? state.fetched[table].data : [])
+  }
 }
 
 export default {
