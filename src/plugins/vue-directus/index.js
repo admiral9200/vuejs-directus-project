9f169844817179bd6eb07a890674685a1fde7b68
Nicholas Wong <lion9200.world@gmail.com>
2018-02-04 14:11:35 +0100

    change error strings

--- src/plugins/vue-directus/index.js
+++ src/plugins/vue-directus/index.js
@@ -5,11 +5,11 @@ import * as VueDirectusComponents from './components'
 const VueDirectus = {
   install(Vue, { store }) {
     if (!store) {
-      throw Error('VueDirectus requires the vuex store.')
+      throw Error(`VueDirectus requires the vuex store.`)
     }
 
     if (!VueDirectusApi) {
-      throw Error('VueDirectus requires the directus-sdk client.')
+      throw Error(`VueDirectus requires the directus-sdk client.`)
     }
 
     // Register new store module
