193e429fca083f58206a54f0b3625633b60ed80c
Nicholas Wong <lion9200.world@gmail.com>
2018-02-06 18:57:58 +0100

    add quill editor to text component

--- src/plugins/vue-directus/components/modules/VueDirectusText.vue
+++ src/plugins/vue-directus/components/modules/VueDirectusText.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vue-directus-text">
     <div class="vue-directus-text__slot">
-      <div v-html="text" />
+      <quill-editor :content="renderedText" :options="options" @change="change($event)" @ready="ready($event)" />
     </div>
   </div>
 </template>
@@ -14,6 +14,57 @@ export default {
     text: {
       type: String,
       default: ''
+    },
+    html: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      rawText: '',
+      renderedText: '',
+      options: {
+        theme: 'bubble',
+        placeholder: 'Enter some text...',
+        modules: {
+          imageDrop: true,
+          imageResize: {
+            modules: ['Resize', 'DisplaySize', 'Toolbar']
+          },
+          clipboard: {
+            matchVisual: false
+          },
+          toolbar: this.html
+            ? [
+                [{ header: [1, 2, false] }],
+                ['bold', 'italic'],
+                [{ color: [] }, { background: [] }],
+                [{ align: [] }],
+                ['link', 'image'],
+                ['clean']
+              ]
+            : false
+        }
+      }
+    }
+  },
+
+  created() {
+    this.renderedText = this.text
+  },
+
+  methods: {
+    ready(quill) {
+      this.rawText = quill.container.firstChild.innerHTML
+    },
+    change({ html, text }) {
+      if (html !== this.rawText) {
+        console.log('changed')
+      } else {
+        console.log('unchanged')
+      }
     }
   }
 }
