9db53e8273eab0524a19172f2e1aee4755e8ecf2
Nicholas Wong <lion9200.world@gmail.com>
2018-02-07 13:59:52 +0100

    add image component

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
         <vue-directus-item v-for="project in items('projects')" table="projects" :id="project._id" :key="project._id">
           <vue-directus-text :column="'name'" :placeholder="'Placeholder test...'" :text="project.name" />
           <vue-directus-text :html="true" :column="'description'" :text="project.description" />
+          <vue-directus-image :name="project.image.data.name" :height="project.image.data.height" :width="project.image.data.width" />
         </vue-directus-item>
       </vue-directus-collection>
     </vue-directus-app>
---
+++ src/plugins/vue-directus/components/modules/VueDirectusImage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="vue-directus-image">
+    <div class="vue-directus-image__slot">
+      <vue-croppie ref="croppie" :mouse-wheel-zoom="false" :viewport="viewport" :boundary="boundary" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueCroppie from 'vue-croppie'
+
+Vue.use(VueCroppie)
+
+export default {
+  name: 'VueDirectusImage',
+
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: Number,
+      default: 100
+    },
+    height: {
+      type: Number,
+      default: 100
+    }
+  },
+
+  data() {
+    return {
+      cropped: null,
+      boundary: { width: this.width, height: this.height },
+      viewport: { width: this.width - 10, height: this.height - 10 }
+    }
+  },
+
+  mounted() {
+    this.$refs.croppie.bind({
+      url: `http://192.168.33.6/storage/uploads/${this.name}`
+    })
+  }
+}
+</script>
