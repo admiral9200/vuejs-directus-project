9b8a569934c8b15ca8b3ac8373cce5315593b134
Nicholas Wong <lion9200.world@gmail.com>
2018-02-14 20:36:08 +0100

    add fallback when no items

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <vue-directus-app>
-      <vue-directus-collection table="projects" :items="itemsByTable('projects')">
+      <vue-directus-collection table="projects" :items="itemsByTable('projects')" v-if="itemsByTable('projects').length > 0">
         <vue-directus-item v-for="item in itemsByTable('projects')" table="projects" :id="item._id" :key="item._id">
           <vue-directus-text column="name" placeholder="Enter a name..." :text="item.name" />
           <vue-directus-text column="description" :rich="true" placeholder="Enter a description..." :text="item.description" />
           <vue-directus-image column="image" :img="item.image" v-if="item.image" />
         </vue-directus-item>
       </vue-directus-collection>
+      <div v-else>
+        Failed to load projects.
+      </div>
     </vue-directus-app>
   </div>
 </template>
